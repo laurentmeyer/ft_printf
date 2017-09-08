754f95e063e16a1c67580d021ed53297cdf7a15e
lmeyer <laurent.dubois.meyer@gmail.com>
2017-09-08 10:08:36 +0200

    color testing in the main.c testfile

--- main.c
+++ main.c
@@ -23,6 +23,15 @@
 printf(__VA_ARGS__)
 #define ERR -1
 
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define BLU   "\x1B[34m"
+#define MAG   "\x1B[35m"
+#define CYN   "\x1B[36m"
+#define WHT   "\x1B[37m"
+#define RESET "\x1B[0m"
+
 
 int		main(void)
 {
@@ -30,9 +39,11 @@ int		main(void)
 	int	rot;
 
 	setlocale(LC_ALL, "");
-	ret = printf("cc_d = |%.4c|\n==========\n", 0);
+	//ret = printf("cc_d = |%.4c|\n==========\n");
+	ret = printf("j'aime le " RED "rouge " RESET " et le bleu" RESET "\n===\n");
 	printf(" ret = %d\n==========\n", ret);
-	rot = ft_printf("ft_d = |%.4c|\n==========\n", 0);
+	//rot = ft_printf("ft_d = |%.4c|\n==========\n", 0);
+	rot = printf("j'aime le " RED "rouge " RESET " et le bleu" RESET "\n===\n");
 	printf(" rot = %d\n==========\n", rot);
 	return (0);
 }
